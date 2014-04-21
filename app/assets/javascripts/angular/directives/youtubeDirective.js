96e2326e40265955591d791f2f7a1d90e0d78694
Haroon Rasheed <haroonrasheed@berkeley.edu>
2014-04-21 14:02:53 -0700

    [CLC-2999] Lazy load the youtube videos

---
+++ app/assets/javascripts/angular/directives/youtubeDirective.js
@@ -0,0 +1,20 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('calcentral.directives').directive('ccYoutubeDirective', function($sce) {
+    return {
+      restrict: 'AE',
+      scope: {link: '='},
+      replace: true,
+      template: '<iframe type="text/html" width="100%" height="100%" src="{{url}}" frameborder="0" allowfullscreen></iframe>',
+      link: function(scope) {
+          scope.$watch('link', function(urlValue) {
+             if (urlValue) {
+                 scope.url = $sce.trustAsResourceUrl(urlValue);
+             }
+          });
+      }
+    };
+  });
+
+})(window.angular);
